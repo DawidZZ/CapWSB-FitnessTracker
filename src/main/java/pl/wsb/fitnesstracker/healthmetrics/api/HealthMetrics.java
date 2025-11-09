5192d6bacb8683ee56181c415ae685bf807ac28e
Dawid Ziobrowski <dawidziobrowski.dz@gmail.com>
2025-11-09 15:21:57 +0100

    Add health metrics, add relation user-statistics

---
+++ src/main/java/pl/wsb/fitnesstracker/healthmetrics/api/HealthMetrics.java
@@ -0,0 +1,31 @@
+package pl.wsb.fitnesstracker.healthmetrics.api;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import pl.wsb.fitnesstracker.user.api.User;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "health_metrics")
+public class HealthMetrics {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Column
+    private Float weight;
+
+    @Column
+    private Float height;
+
+    @Column(name = "heart_rate")
+    private Integer heartRate;
+
+
+}
--- src/main/java/pl/wsb/fitnesstracker/user/api/User.java
+++ src/main/java/pl/wsb/fitnesstracker/user/api/User.java
@@ -6,8 +6,11 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
+import pl.wsb.fitnesstracker.healthmetrics.api.HealthMetrics;
+import pl.wsb.fitnesstracker.statistics.api.Statistics;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @Entity
 @Table(name = "users")
@@ -26,6 +29,12 @@ public class User {
 
     @Column(nullable = false, unique = true)
     private String email;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<HealthMetrics>  healthMetrics;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Statistics> statistics;
 
     public User(
             final String firstName,
