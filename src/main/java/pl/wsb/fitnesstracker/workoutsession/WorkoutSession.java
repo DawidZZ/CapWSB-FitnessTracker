b75c89849e9fda22d9af870c3d0ffdee48178bb7
Joanna Wasilenko <joannawasilenko@Joannas-MacBook-Air.local>
2025-11-23 13:57:57 +0100

    add workout session

--- src/main/java/pl/wsb/fitnesstracker/workoutsession/WorkoutSession.java
+++ src/main/java/pl/wsb/fitnesstracker/workoutsession/WorkoutSession.java
@@ -1,17 +1,40 @@
 package pl.wsb.fitnesstracker.workoutsession;
 
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import pl.wsb.fitnesstracker.training.api.Training;
 
-// TODO: Define the Event entity with appropriate fields and annotations
+
+@Entity
+@Getter
+@Setter
+@Table(name = "workout_session")
 public class WorkoutSession {
 
     @Id
-    private int id;
-    private int trainingId;
-    private String timestamp;
-    private double startLatitude;
-    private double startLongitude;
-    private double endLatitude;
-    private double endLongitude;
-    private double altitude;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "training_id", nullable = false)
+    private Training training;
+
+    @Column(name = "timestamp", nullable = false)
+    private java.time.LocalDateTime timestamp;
+
+    @Column(name = "start_latitude", nullable = false)
+    private Double startLatitude;
+
+    @Column(name = "start_longitude", nullable = false)
+    private Double startLongitude;
+
+    @Column(name = "end_latitude")
+    private Double endLatitude;
+
+    @Column(name = "end_longitude")
+    private Double endLongitude;
+
+    @Column(name = "altitude")
+    private Double altitude;
 }
