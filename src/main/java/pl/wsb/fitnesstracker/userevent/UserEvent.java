2e4c36faf6b4209b4fcd7af7c2ce4e184894375d
Dawid Ziobrowski <dawidziobrowski.dz@gmail.com>
2025-12-07 12:47:03 +0100

    Add UserEvent

---
+++ src/main/java/pl/wsb/fitnesstracker/userevent/UserEvent.java
@@ -0,0 +1,21 @@
+package pl.wsb.fitnesstracker.userevent;
+
+import jakarta.persistence.*;
+import pl.wsb.fitnesstracker.event.Event;
+import pl.wsb.fitnesstracker.user.api.User;
+
+@Entity
+@Table(name="user_event")
+public class UserEvent {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "event_id")
+    private Event event;
+}
